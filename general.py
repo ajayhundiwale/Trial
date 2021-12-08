4f980a3099e462494936b893099ec74d8296afaa
Ajay <ajayhundiwale@gmail.com>
2021-12-08 14:54:48 +0530

    08/12/2021

---
+++ general.py
@@ -0,0 +1,17 @@
+def greet(name = "Ajay"):
+      gr = ("Hello " +name)
+      return gr
+
+a = greet ("Shikha")
+print(a)
+
+
+# def percentage (marks):
+#        p = ((marks[0]+ marks[1] + marks[2]+ marks[3])/400)*100
+#        return p
+      
+
+
+# marks1 = [60,40,80,95]
+# pr = percentage(marks1)
+# print(pr)
---
+++ reverse_multi_table.py
@@ -0,0 +1,3 @@
+num = int(input("Enter number: "))
+for i in range (num*10, 0,-num):
+       print(i)
